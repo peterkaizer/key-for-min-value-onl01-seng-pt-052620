0089ff87df3f8e2834dbd23b927f99d4ec3f794f
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-08 11:14:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name)
+def key_for_min_value(hash)
   lowest_value = 0
- lowest_key = nil
+  lowest_key = nil
 end
 
 # def key_for_min_value(name_hash)
