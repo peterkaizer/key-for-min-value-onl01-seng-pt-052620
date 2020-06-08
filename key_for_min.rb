91c6147dc7f82ff74a9451d41e84267cf8ff2530
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-08 11:11:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_value = 0
+ lowest_key = nil
 end
+
+# def key_for_min_value(name_hash)
+#   key_for_min_value < |name_hash|
+#   lowest_value = 0
+#   lowest_key = nil
+# end
+
+#key_for_min_value < |name_hash|
+
